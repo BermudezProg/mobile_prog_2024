0db9e639ef4da3d41f5c5d63f9b28838a27721a5
James <2002263@ub.edu.ph>
2024-09-07 10:58:06 +0800

    new test

---
+++ Documents/Bermudez/test.js
@@ -0,0 +1,20 @@
+console.log ("Demo Git Changes");
+
+//add new project and using it.
+/*
+1. create a new repo on Github
+2. go to the folder where your project is located
+3. run git init
+4. git add
+5. git commit -m "first commit"
+6. run git branch -M main
+7. run git remote add origin <url>
+8. run git push -u origin main
+
+add/edit, commit and push
+
+1. git add . 
+2. git commit -m "Demo GIT change:
+3. git push
+
+*/
